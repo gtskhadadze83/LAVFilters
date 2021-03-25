5839b54eb2382e64235c3cc79be10c4f3dfbd8cb
gtskhadadze83 <53004769+gtskhadadze83@users.noreply.github.com>
2021-03-25 15:56:38 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,10 +7,6 @@ for /f "usebackq tokens=*" %%i in (`call "%ProgramFiles(x86)%\Microsoft Visual S
 set "VSCMD_START_DIR=%CD%"
 call "%VSInstallDir%\Common7\Tools\VsDevCmd.bat" -no_logo -arch=x86 > NUL
 
-sh build_ffmpeg.sh x86 || EXIT /B 1
-MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
-IF ERRORLEVEL 1 EXIT /B 1
-
 sh build_ffmpeg.sh x64 || EXIT /B 1
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=x64
 IF ERRORLEVEL 1 EXIT /B 1
